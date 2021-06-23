7f427ef2774a987e676afdfd41be5050173d85a0
TheVicio <thevicio27@gmail.com>
2021-06-23 16:36:56 -0300

    Aplicado OO na interface de saves.

---
+++ classes/SaveI.gd
@@ -0,0 +1,35 @@
+extends VBoxContainer
+
+class_name SaveI
+
+func _init(slot):
+	var slot_label = Label.new() 
+	slot_label.text = "Slot"
+	var hboxcontainer = HBoxContainer.new()
+	var save_button = Button.new()
+	save_button.name = "SaveButton"
+	var delete_button = Button.new()
+	delete_button.name = "DeleteButton"
+	save_button.connect("button_down", self, "on_save_pressed", [slot])
+	delete_button.connect("button_down", self, "on_delete_pressed", [slot])
+	# set language
+	if Directory.new().file_exists(slot):
+		save_button.text = Options.lang_content["load"]
+	else:
+		save_button.text = Options.lang_content["new_game"]
+		delete_button.visible = false
+	add_child(slot_label)
+	add_child(hboxcontainer)
+	hboxcontainer.add_child(save_button)
+	hboxcontainer.add_child(delete_button)
+
+
+func on_save_pressed(slot):
+	Playervariables.set_slot(slot)
+	get_tree().change_scene("res://Jogo.tscn")
+
+
+func on_delete_pressed(slot):
+	Directory.new().remove(slot)
+	$SaveButton.text = Options.lang_content["new_game"]
+	$DeleteButton.visible = false
